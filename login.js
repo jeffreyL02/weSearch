b48a7d572a1d83aeb565ca9a8250fd5c498b79ce
NovaSagittarii <novasagittarii@gmail.com>
2019-08-28 17:52:17 -0600

    firebases

--- login.js
+++ login.js
@@ -1,3 +1,18 @@
-document.getElementById("signUp").addEventListener("click",function(){
+import data from './init.mjs';
+firebase.initializeApp(data);
+
+const AUTH = firebase.auth();
+
+document.getElementById("signUp").addEventListener("click", function(){
   location.assign("signup.html");
+});
+
+let loginPassword = document.getElementById("password");
+document.getElementById("submit").addEventListener("click", () => {
+  AUTH.signInWithEmailAndPassword(document.getElementById("username").value, loginPassword.value).then(() => {
+    window.location.href = "listings.html";
+  }).catch(e => {
+    alert("Login failed!");
+    loginPassword.value = "";
+  });
 });
--- signup.js
+++ signup.js
@@ -1,3 +1,21 @@
-document.getElementById("login").addEventListener("click",function(){
+import data from './init.mjs';
+firebase.initializeApp(data);
+
+const AUTH = firebase.auth();
+
+document.getElementById("login").addEventListener("click", function(){
   location.assign("index.html");
+});
+
+const inputs = document.getElementsByTagName('input');
+
+document.getElementById("register").addEventListener("click", () => {
+  if (inputs[3].value === inputs[4].value){
+		AUTH.createUserWithEmailAndPassword(inputs[1].value, inputs[2].value).then(() => {
+		window.location.href = "listings.html";
+		}).catch(e => alert(e));
+  }else{
+    alert("Password Mismatch!!");
+    inputs[3].value = inputs[4].value = "";
+  }
 });
