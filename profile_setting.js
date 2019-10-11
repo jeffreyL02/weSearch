12205cbd226a221a01596b797916087a36c0beea
lillee205 <leelillian205@gmail.com>
2019-10-11 15:21:49 -0700

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- profile_setting.js
+++ profile_setting.js
@@ -1,10 +1,9 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
 
-for (i = 0; i < coll.length; i++) {
+//expand collapisble
+var coll = document.getElementsByClassName("collapsible");
+for (let i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     var content = this.nextElementSibling;
-    console.log(content)
     if (content.style.maxHeight!="0px"){
       content.style.maxHeight = "0";
       content.style.padding="0 3vh 0 3vh";
