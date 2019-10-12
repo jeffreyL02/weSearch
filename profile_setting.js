aff0088db9d8cd74d58614d7975528812154a16d
lillee205 <leelillian205@gmail.com>
2019-10-12 12:49:01 -0700

    Adaf

--- profile_setting.js
+++ profile_setting.js
@@ -8,8 +8,8 @@ for (let i = 0; i < coll.length; i++) {
       content.style.maxHeight = "0";
       content.style.padding="0 3vh 0 3vh";
     } else {
-        content.style.maxHeight = "17vh";
-        content.style.padding= "1.5% 3% 1.5% 3%";
+        content.style.maxHeight = "500vh";
+        content.style.padding= "1.5vh 3vh 1.5vh 3vh";
       }
   });
 }
